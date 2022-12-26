ff4dc306590be7465308cfeb35a3c3eabadeee0e
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-25 21:09:12 -0800

    Staff Section Shell

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -2,11 +2,13 @@ import React, {useState} from 'react'
 import Styles from "../Styles/Page-Styles/Landing.module.scss"
 
 import Hero from '../Page-Sections/Hero'
+import Staff from '../Page-Sections/Staff'
 
 const Landing = () => {
   return (
     <div className={Styles.Page}>
       <Hero />
+      <Staff />
     </div>
   )
 }
