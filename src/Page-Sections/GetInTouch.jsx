fddd3e449ed8b88cf4ad484805dcb1e84d9b0079
Daniel Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-12-27 14:56:30 -0500

    Get in touch form added and is responsive

--- src/Page-Sections/GetInTouch.jsx
+++ src/Page-Sections/GetInTouch.jsx
@@ -5,7 +5,7 @@ import Styles from "../Styles/Page-Section-Styles/GetInTouch.module.scss";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 // Material UI Icons Import End
 
 const GetInTouch = () => {
@@ -28,34 +28,82 @@ const GetInTouch = () => {
       <div className={Styles.Social_Icon_Bar}>
         <div className={Styles.Social_Container}>
           <a href={`#`} target={"_blank"} rel={"noreferrer"}>
-            <InstagramIcon className={Styles.SocialIcon} sx={{ color: "white", fontSize: 30 }} />
+            <InstagramIcon
+              className={Styles.SocialIcon}
+              sx={{ color: "white", fontSize: 30 }}
+            />
             <p className={Styles.Social_Title}>Instagram</p>
           </a>
         </div>
 
         <div className={Styles.Social_Container}>
           <a href={`#`} target={"_blank"} rel={"noreferrer"}>
-            <FacebookIcon className={Styles.SocialIcon} sx={{ color: "white", fontSize: 30 }} />
+            <FacebookIcon
+              className={Styles.SocialIcon}
+              sx={{ color: "white", fontSize: 30 }}
+            />
             <p className={Styles.Social_Title}>FaceBook</p>
           </a>
         </div>
 
         <div className={Styles.Social_Container}>
           <a href={`#`} target={"_blank"} rel={"noreferrer"}>
-            <TwitterIcon className={Styles.SocialIcon} sx={{ color: "white", fontSize: 30 }} />
+            <TwitterIcon
+              className={Styles.SocialIcon}
+              sx={{ color: "white", fontSize: 30 }}
+            />
             <p className={Styles.Social_Title}>Twitter</p>
           </a>
         </div>
 
         <div className={Styles.Social_Container}>
           <a href={`#`} target={"_blank"} rel={"noreferrer"}>
-            <WhatsAppIcon className={Styles.SocialIcon} sx={{ color: "white", fontSize: 30 }} />
+            <WhatsAppIcon
+              className={Styles.SocialIcon}
+              sx={{ color: "white", fontSize: 30 }}
+            />
             <p className={Styles.Social_Title}>WhatsApp</p>
           </a>
         </div>
       </div>
 
+      <div className={Styles.Form_Container}>
+        <form action="" className={Styles.Form}>
+          <fieldset className={Styles.Name__Eamil}>
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              className={Styles.Input}
+            />
+            <input
+              type="Email"
+              placeholder="Email"
+              required
+              className={Styles.Input}
+            />
+          </fieldset>
+          <input
+            type="Text"
+            placeholder="Subject"
+            required
+            className={`${Styles.Input} ${Styles.Subject}`}
+          />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+            required
+            className={`${Styles.Input} ${Styles.Message}`}
+          ></textarea>
 
+          <button type={"submit"} className={Styles.Submit_Button}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
